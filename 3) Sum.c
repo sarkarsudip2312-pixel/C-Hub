0ddf425fd63c87da0bfcec830cba9da17335a939
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-08-17 14:11:40 +0600

    Add files via upload

---
+++ 3) Sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (){
+    
+    int a, b, sum;
+    
+    printf("Enter 1st Number: ");
+    scanf("%d", &a);
+    
+    printf("Enter 2nd Number: ");
+    scanf("%d", &b);
+    
+    sum= a+b;
+    
+    printf("Your Answer is: %d", sum);
+   
+    
+    return 0;
+}
