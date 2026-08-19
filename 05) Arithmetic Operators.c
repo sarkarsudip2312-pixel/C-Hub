4f3f7388ba91e46d7e981b4f4f341ebf9d097d47
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-08-20 02:07:22 +0600

    Add files via upload

---
+++ 05) Arithmetic Operators.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+    
+    int a,b;
+    
+    printf("Enter 1st Number: ");
+    scanf("%d",&a);
+    
+    printf("Enter 2nd Number: ");
+    scanf("%d",&b);
+    
+    printf("Your Answer is: %d\n", a+b);
+    printf("Your Answer is: %d\n", a-b);
+    printf("Your Answer is: %d\n", a*b);
+    printf("Your Answer is: %d\n", a/b);
+    return 0;
+}
