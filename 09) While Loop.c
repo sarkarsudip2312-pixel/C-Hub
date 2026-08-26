13991d9b4ecc69232d0bb11d7a2d66679e03ce53
Sudip Sarkar <sarkarsudip2312@gmail.com>
2026-08-27 01:11:00 +0600

    Add files via upload

---
+++ 09) While Loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main () {
+     
+     int s=1;
+     
+     
+     while(s<=6){
+         
+         printf("%d\n", s);
+         
+         s++;
+     }
+    
+    return 0;
+}
